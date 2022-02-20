b28ecae0f7ec6a15d9c12fee3168e73fb1c7035c
Romulo Nunes de Menezes Roriz <rnmroriz@gmail.com>
2022-02-20 14:36:27 +0100

    Created User model & Register validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,11 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+
 // @desc Register new user
 // @route /api/users
 // @access public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
 
 	// Validation
@@ -10,15 +14,42 @@ const registerUser = (req, res) => {
 		throw new Error('Please include all fields');
 	}
 
-	res.send('Register Route');
-};
+	// Find if user already exists
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400);
+		throw new Error('User already exists');
+	}
+
+	// Hash password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	// Create user
+	const user = await User.create({
+		name: name,
+		email: email,
+		password: hashedPassword,
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
+});
 
 // @desc Login a user
 // @route /api/users/login
 // @access public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
 	res.send('Login Route');
-};
+});
 
 module.exports = {
 	registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const errorHandler = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
 
 const app = express();
 
