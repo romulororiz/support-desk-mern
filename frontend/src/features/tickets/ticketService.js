5e22b1384788d99d547752d2d071144c6b0da7d3
Romulo Nunes de Menezes Roriz <rnmroriz@gmail.com>
2022-02-27 19:20:18 +0100

    Added single ticket route and single ticket UI/functionality

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -23,12 +23,27 @@ const getTickets = async token => {
 	};
 	const response = await axios.get(API_URL, config);
 
-	return response.data
+	return response.data;
+};
+
+// Get single ticket
+const getTicket = async (ticketId, token) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+	const response = await axios.get(`${API_URL}/${ticketId}`, config);
+
+	// console.log(response.data);
+
+	return response.data;
 };
 
 const ticketService = {
 	createTicket,
 	getTickets,
+	getTicket,
 };
 
 export default ticketService;
--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -30,13 +30,32 @@ export const createTicket = createAsyncThunk(
 	}
 );
 
-// Fetch tickets
+// Get tickets
 export const getTickets = createAsyncThunk(
 	'ticket/getAll',
-	async (ticketData, thunkAPI) => {
+	async (_, thunkAPI) => {
 		try {
 			const token = thunkAPI.getState().auth.user.token;
 			return await ticketService.getTickets(token);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+// Get tickets
+export const getTicket = createAsyncThunk(
+	'ticket/get',
+	async (ticketId, thunkAPI) => {
+		try {
+			const token = thunkAPI.getState().auth.user.token;
+			return await ticketService.getTicket(ticketId, token);
 		} catch (error) {
 			const message =
 				(error.response &&
@@ -79,6 +98,19 @@ export const ticketSlice = createSlice({
 				state.tickets = action.payload;
 			})
 			.addCase(getTickets.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			})
+			.addCase(getTicket.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(getTicket.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.ticket = action.payload;
+			})
+			.addCase(getTicket.rejected, (state, action) => {
 				state.isLoading = false;
 				state.isError = true;
 				state.message = action.payload;
